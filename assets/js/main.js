fbb26628e1498eaa4b2a1e3c7518c0b57f3f35dc
Artem-Andreevich <ops210@mail.ru>
2024-07-24 22:28:26 +0500

    add burger-menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,35 @@
 window.addEventListener('load', function(){
 
 	const swiper = new Swiper('.swiper', {
-		loop: true,
-		slidesPerView: 4.3,
-		spaceBetween: 32,
+		slidesPerView: 'auto',
+		spaceBetween: 8,
 		grabCursor: true,
-		loopPreventsSlide: false,
-	  });
+		breakpoints: {
+			756: {
+			  spaceBetween: 20
+			},
+			991: {
+				spaceBetween: 32
+			  },
+		  },
+	});
+
+	const body = document.querySelector('body')
+	const burgerButton = document.querySelector('.js-burger')
+	const burgerMenu = document.querySelector('.js-burger-menu')
+	const burgerMenuClose = document.querySelectorAll('.js-burger-menu-close')
+
+	burgerButton.addEventListener('click', function(){
+		burgerMenu.classList.add('open')
+		body.classList.add('open-menu')
+
+	})
+	burgerMenuClose.forEach( btn => {
+		btn.addEventListener('click', function(){
+			burgerMenu.classList.remove('open')
+			body.classList.remove('open-menu')
+		})
+	})
 	
+
 })
