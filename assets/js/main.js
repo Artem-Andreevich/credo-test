36f4cb186b34a58e85996e3fb0dac507d0250dc3
ARTEM ANDREEVICH <ops210@mail.ru>
2024-07-24 15:22:57 +0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function(){
+
+	const swiper = new Swiper('.swiper', {
+		loop: false,
+		slidesPerView: 4.2,
+		spaceBetween: 30,
+		grabCursor: true,
+		freeMode: true,
+	  });
+	
+})
